ace4662906bf1de70f53236c1c33fde20f6d325e
Luis Vespa <vespasoft@gmail.com>
2017-02-21 08:58:28 -0400

    se completo el chapter Rounting V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,30 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+// --------------------------  COMPONENT -----------------------------
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './special-super-hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  imports:      [
+  imports: [
     BrowserModule,
-    FormsModule
-   ],
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
